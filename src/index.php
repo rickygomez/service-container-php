4ff75e0e0860297559d756c72b10406b142ba59f
Ricardo-Gomez <ricardo@assys.com.br>
2023-05-24 15:21:59 -0300

    # Implement dependency injection

--- src/index.php
+++ src/index.php
@@ -1,6 +1,11 @@
 <?php
+
+use App\Models\Logger;
+use App\Models\Storage;
+use App\Models\User;
+
 require_once __DIR__ . '/../vendor/autoload.php';
-$user = new App\Models\User();
+$user = new User(new Storage(new Logger()));
 $user->name = 'test';
 $user->email = 'test@teste.com';
 $user->avatar = 'test.png';
